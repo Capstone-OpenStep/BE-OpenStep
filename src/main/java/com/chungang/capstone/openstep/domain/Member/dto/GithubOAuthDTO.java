f6392aa7c38f5da710bd2a471b8cf0274f00ee4a
Miging <dnfldpden32@gmail.com>
2025-05-03 19:05:53 +0900

    feat: github 응답/요청 DTO 생성

---
+++ src/main/java/com/chungang/capstone/openstep/domain/Member/dto/GithubOAuthDTO.java
@@ -0,0 +1,22 @@
+package com.chungang.capstone.openstep.domain.Member.dto;
+
+import lombok.Getter;
+
+public class GithubOAuthDTO {
+
+	public record GithubOAuthReq(
+		String client_id,
+		String client_secrete,
+		String code
+	) {}
+
+	public record GithubOauthRes(
+		String access_token,
+		String token_type,
+		String scope
+	) {}
+
+	public record GithubUserInfoRes(
+		String name
+	){}
+}
