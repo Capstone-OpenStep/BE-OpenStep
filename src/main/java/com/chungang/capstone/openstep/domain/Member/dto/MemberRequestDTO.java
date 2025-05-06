523f2a34de4abad6153b2f1df35652d3118a7cfc
Miging <dnfldpden32@gmail.com>
2025-05-06 21:39:02 +0900

    feat: 응답/요청 DTO 생성

--- src/main/java/com/chungang/capstone/openstep/domain/Member/dto/MemberRequestDTO.java
+++ src/main/java/com/chungang/capstone/openstep/domain/Member/dto/MemberRequestDTO.java
@@ -1,4 +1,13 @@
 package com.chungang.capstone.openstep.domain.Member.dto;
 
+import java.util.List;
+
 public class MemberRequestDTO {
+	public record UpdateDomainsReq(
+		List<String> domains
+	) { }
+
+	public record UpdateSkillsReq(
+		List<String> skills
+	){}
 }
