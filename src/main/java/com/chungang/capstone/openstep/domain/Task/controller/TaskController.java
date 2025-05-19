b0058751f2883b047bb25e2e1156187ba4e747c3
Miging <dnfldpden32@gmail.com>
2025-05-19 12:35:54 +0900

    refactor: 서비스로직 변경
    컨버터 로직이 서비스에서 돌아가독록 변경

--- src/main/java/com/chungang/capstone/openstep/domain/Task/controller/TaskController.java
+++ src/main/java/com/chungang/capstone/openstep/domain/Task/controller/TaskController.java
@@ -14,6 +14,7 @@ import com.chungang.capstone.openstep.global.apiPayload.ApiResponse;
 import com.chungang.capstone.openstep.global.apiPayload.code.status.SuccessStatus;
 import com.chungang.capstone.openstep.global.security.util.SecurityUtils;
 
+import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 
@@ -25,14 +26,23 @@ public class TaskController {
 
 	private final TaskQueryService taskQueryService;
 
+	@Operation(summary = "특정 테스크 상세 조회 API", description = "특정 오픈소스 레포지토리의 기여(테스크) 정보를 조회합니다.")
 	@GetMapping("/{task-id}")
 	public ApiResponse<TaskResponseDTO.TaskDetail> getTaskDetail(
 		@PathVariable("task-id") Long taskId
 	) {
 		Member member= SecurityUtils.getCurrentMember();
-		Task task=taskQueryService.getTaskById(taskId,member);
+		return ApiResponse.onSuccess(SuccessStatus.TASK_GET_OK, taskQueryService.getTaskDetailById(taskId,member));
+	}
 
-		return ApiResponse.onSuccess(SuccessStatus.TASK_GET_OK, TaskConverter.toTaskDetail(task));
+	@Operation(summary = "특정 테스크의 브랜치 이름 조회 API", description = "특정 오픈소스 레포지토리의 기여(테스크) 브랜치 이름을 조회합니다.")
+	@GetMapping("/{task-id}/branch")
+	public ApiResponse<TaskResponseDTO.TaskBranchName> getTaskBranch(
+		@PathVariable("task-id") Long taskId
+	) {
+		Member member= SecurityUtils.getCurrentMember();
+
+		return ApiResponse.onSuccess(SuccessStatus.TASK_BRANCH_GET_OK, taskQueryService.getBranchNameByTask(taskId,member));
 	}
 
 }
